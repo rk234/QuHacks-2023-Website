a54a5eb6125b479537f51afe04e164f94b3eb2eb
Ramy Kaddouri <106203063+rk234@users.noreply.github.com>
2023-09-03 10:50:35 -0400

    fix typo

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.announcementBanner}>
-        <p>📢  Want to help make QuHacks possible? Donate here <a href="https://hcb.hackclub.com/donations/start/quhacks" rel="noreferrer noopener" target="_blank">HERE</a>!</p>
+        <p>📢  Want to help make QuHacks possible? Donate <a href="https://hcb.hackclub.com/donations/start/quhacks" rel="noreferrer noopener" target="_blank">HERE</a>!</p>
       </div>
       <div className={styles.homecontainer}>
         <Image src="/logo.png" width={100} height={100} sizes='(max-width: 750px) 15rem, 20rem' className={styles.logo} />
