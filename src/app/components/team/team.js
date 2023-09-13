0f63df7b3d07248d5d026618b235e3c4a1aa3b8f
devsai9 <saisiddhish@gmail.com>
2023-09-12 21:22:14 -0400

    Added more people to team section

--- src/app/components/team/team.js
+++ src/app/components/team/team.js
@@ -5,18 +5,31 @@ import Link from 'next/link';
 export default function TeamSection() {
     let teamCards = [
         ["Aryan Sharma", "Director", "/headshots/aryan_headshot.jpg"],
+        ["Seonyoung Lee", "Director", "/headshots/seonyoung_headshot.jpg"],
+
+        ["Neeraj L", "Secretary", "/headshots/neeraj_headshot.jpg"],
+
+        ["Caleb Shim", "Treasurer", "/headshots/caleb_headshot.jpg"],
+
         ["Ramy Kaddouri", "Website Team Lead", "/headshots/ramy_headshot.jpeg"],
+        ["Joel L", "Website Dev", "/headshots/joel_headshot.jpg"],
         ["Sai Chandra", "Website Dev", "/headshots/sai_siddhish_headshot.png"],
         ["Nicole Luo", "Website Dev", "/headshots/nicole_headshot.png"],
+
+        ["Gordon L", "Outreach Team", "/headshots/gordon_headshot.png"],
+
+        ["Akhil L", "Sponsor Liason", "/headshots/akhil_headshot.jpg"],
+
+        ["Jaden  Li", "Workshop Creator", "/headshots/jaden_headshot.jpg"],
+        ["Victor Liu", "Workshop Creator", "/headshots/victor_headshot.jpg"],
+
         ["Ishani Ghosh", "TBD", "/headshots/ishani_headshot.png"],
-        ["Jaden  Li", "TBD", "/headshots/jaden_headshot.jpg"],
-        ["Victor Liu", "TBD", "/headshots/victor_headshot.jpg"],
         ["Luis Valerio", "TBD", "/headshots/luis_headshot.png"],
     ]
 
     teamCards = teamCards.map((card) => {
         return (
-            <TeamCard name={card[0]} position={card[1]} image={card[2]} />
+            <TeamCard name={card[0]} position={card[1]} image={card[2]} key={card[0]} />
         )
     });
 
