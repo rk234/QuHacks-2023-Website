acb83504e14dacbfc3ef676b533f14079e28dab7
Ramy K <myramkadd@gmail.com>
2023-09-22 23:35:18 -0400

    timeline + sponsor additions

--- src/app/timeline/page.js
+++ src/app/timeline/page.js
@@ -6,38 +6,31 @@ export default function Timeline() {
             <div className={styles.headContainer}>
                 <div className={styles.head}>
                     <div className={styles.headtext}>
-                        <h1>What we do</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum varius duis at consectetur lorem donec massa sapien. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Malesuada proin libero nunc consequat interdum varius. Cursus sit amet dictum sit amet justo donec enim diam. Vitae nunc sed velit dignissim sodales ut eu. Amet justo donec enim diam vulputate ut pharetra sit. Integer quis auctor elit sed.</p>
+                        <h1>Timeline</h1>
                     </div>
-                    {/*The QH is just a placeholder for now*/}
-                    <h1 className={styles.qh}>QH</h1>
                 </div>
             </div>
             <div className={styles.section}>
                 <div className={styles.timeline}>
                     <div className={styles.timelineCard}>
                         <div className={styles.imgContainer}>
-                            <img src="preview-card.png" />
+                        <   img src="timeline/pic-2018.jpg" />
                         </div>
-                        <div>
-                            <header className={styles.cardHeader}>Timeline Card Title</header>
+                        <div className={styles.leftContainer}>
+                            <header className={styles.cardHeader}>QuHacks 2018</header>
                             <div className={styles.textContainer}>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, quas
-                                ex vero enim in doloribus officiis ullam vel nam esse sapiente velit
-                                incidunt. Eaque quod et, aut maiores excepturi sint.
+                                Held at Towson University on December 8th, 2018.
                             </div>
                         </div>
                     </div>
                     <div className={styles.timelineCard}>
                         <div className={styles.imgContainer}>
-                            <img src="background.jpg" />
+                            <img src="timeline/pic-2017.jpg" />
                         </div>
-                        <div>
-                            <header className={styles.cardHeader}>Timeline Card Title</header>
+                        <div className={styles.leftContainer}>
+                            <header className={styles.cardHeader}>QuHacks 2017</header>
                             <div className={styles.textContainer}>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, quas
-                                ex vero enim in doloribus officiis ullam vel nam esse sapiente velit
-                                incidunt. Eaque quod et, aut maiores excepturi sint.
+                                The second QuHacks event! Held at UMBC on December 9th, 2017.
                             </div>
                         </div>
                     </div>
