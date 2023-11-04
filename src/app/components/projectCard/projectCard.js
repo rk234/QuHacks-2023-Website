b1cff4a0997965804f31a5975b0d24562a411927
Ramy K <myramkadd@gmail.com>
2023-11-04 01:24:17 -0400

    mostly done with gallery

--- src/app/components/projectCard/projectCard.js
+++ src/app/components/projectCard/projectCard.js
@@ -1,17 +1,17 @@
 import styles from "./component.module.css"
 
-export default function ProjectCard({project, onClick}) {
+export default function ProjectCard({project, onClick, className}) {
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${className}`} onClick={() => onClick(project)}>
             <div className={styles.imgContainer}>
-                <img className={styles.img} src={project.screenshot_url} />
+                <img className={styles.img} src={(project.screenshot_url && project.screenshot_url.trim().length) > 0 ? project.screenshot_url : "placeholder/quhacks-white.png"} />
             </div>
             <div className={styles.head}>
                 <div className={styles.nameContainer}>
                     <h2 className={styles.name}>{project.name}</h2>
 
                     {project.prize ? 
-                        <div className={styles.prizeBadge}>
+                        <div className={styles.badge}>
                             {project.prize.title}
                         </div>
                         : "" 
@@ -22,6 +22,16 @@ export default function ProjectCard({project, onClick}) {
             <div className={styles.descContainer}>
                 <p>{project.description}</p>
             </div>
+            {project.tracks && project.tracks.length > 0 ?
+            <div className={styles.tracksContainer}>
+                Tracks 
+                {project.tracks.map((track, index) => 
+                    <div className={styles.badge}>
+                        {track}
+                    </div>
+                )}
+            </div>
+            : ""}
         </div>
     )
 }
--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -30,18 +30,96 @@ export default function GalleryPage() {
             built_with: "GTK4, Libadwaita, Python",
             public: true,
             prize: {
-                title: "2nd place", rank: 0
+                title: "2nd place", rank: 1
             }
+        },
+        {
+            name: "Demo",
+            description: "demo text demo text demo text demo text demo text demo text demo text",
+            team_members: ["Ramy"],
+            screenshot_url: "",
+            tracks: [],
+            built_with: "Nothing",
+            public: true,
+        },
+        {
+            name: "Demo",
+            description: "demo text demo text demo text demo text demo text demo text demo text",
+            team_members: ["Ramy"],
+            screenshot_url: "",
+            tracks: [],
+            built_with: "Nothing",
+            public: true,
         }
     ])
+    let [showProjectModal, setShowProjectModal] = useState(false)
+    let [project, setProject] = useState()
+    let [query, setQuery] = useState("")
+
+    function showModal(project) {
+        setProject(project)
+        setShowProjectModal(true)
+    }
+
+    function filter(allProjects) {
+        return allProjects
+            .filter(proj => (proj.name+proj.description+proj.team_members.join(" ")).toLowerCase().includes(query))
+            .sort((a, b) => {
+                if(a.prize && b.prize) {
+                    return a.prize.rank-b.prize.rank
+                } else if(a.prize && !b.prize) {
+                    return -1
+                } else if(!a.prize && b.prize) {
+                    return 1
+                } else {
+                    return 0
+                }
+            })
+    }
 
     return (
         <main className={styles.main}>
+            {
+                showProjectModal ?  
+                <div className={styles.modalContainer}>
+                    <div className={styles.modal}>
+                    <div className={styles.modalHeader}>
+                        <h2>{project.name}</h2>
+                        <p>{project.team_members.join(', ')}</p>
+                    </div>
+                    <div className={styles.modalContent}>
+                        <p>{project.description}</p>
+                        <p><em>Built with: {project.built_with}</em></p>
+                        <div className={styles.modalBtns}>
+                        {project.demo ? 
+                            <a href={project.demo} rel="noreferrer noopener" target='_blank'  className={`btn-primary ${styles.demobtn}`}>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+                                </svg>
+                                Try it!
+                            </a>
+                        : ""}
+                        {project.github ? 
+                            <a href={project.github} rel="noreferrer noopener" target='_blank'  className={`btn-secondary ${styles.gitbtn}`}> 
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
+                            </svg>
+
+                            Github 
+                            </a>
+                        : ""}
+                        <button className="btn-secondary" onClick={() => setShowProjectModal(false)}>Close</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                : ""
+            }
             <div className={styles.headerContainer}>
                 <h1>Project Gallery</h1>
                 
                 <div className={styles.searchOptions}>
-                    <input placeholder="Search" className={styles.searchBox} />
+                    <input placeholder="Search" className={styles.searchBox} onChange={(e) => setQuery(e.target.value)} value={query} />
                     <select className={styles.trackSelect}>
                         <option>All Tracks</option>
                         <option>Track 1</option>
@@ -51,8 +129,8 @@ export default function GalleryPage() {
                 </div>
             </div>
             <div className={styles.galleryContainer}>
-                {projects.map(proj =>
-                    <ProjectCard className={styles.project} project={proj} onClick={() => showModal(proj)}></ProjectCard>
+                {filter(projects).map((proj, index) =>
+                    <ProjectCard key={index} className={styles.project} project={proj} onClick={() => showModal(proj)}></ProjectCard>
                 )}
             </div>
         </main>
